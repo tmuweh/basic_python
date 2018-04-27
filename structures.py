734e8b4399cd7fc3d60f5db169cc7ede5533d0ea
Tangue Muweh <atmuweh@gmail.com>
2018-04-27 15:56:48 +0200

    add dictionary codes

--- structures.py
+++ structures.py
@@ -24,3 +24,26 @@ print(players[2:5])
 players[0] = []
 print(players)
 players[:] = []
+print(players)
+
+#shopping
+groceries = {'cereal', 'milk', 'sandwich', 'yogurt', 'lotion', 'beer'}
+print(groceries)
+
+if 'milk' in groceries :
+    print("You already have milk")
+else:
+    print("oh yeah, you need milk")
+
+#dictionary
+
+classmates = {'Tony': 'A male given name', 'Vivian':'A female given name', 'max': 'snabbt mat foretag i Sverige'}
+
+print(classmates)
+for k, v in classmates.items():
+    print(k , v)
+
+#print particular entry in dictionary
+print(classmates['Tony'])
+print(classmates['Vivian'])
+print(classmates['max'])
