e5169019cd46f57d6ac4e7b6c0b0a58265fce6ce
Tangue Muweh <atmuweh@gmail.com>
2018-04-27 10:29:40 +0200

    python's control statements. Loops and conditional statement

---
+++ controls.py
@@ -0,0 +1,10 @@
+
+print ("Hello!");
+
+#if statement
+
+name = "tmuw3h"
+if name is "tmuw3h":
+    print("Your username is tmuw3h");
+elif name != "tmuw3h":
+    print("Hello someone else. Sign up")
