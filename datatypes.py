e7788f142217eac34f254ee8573a3d8350983a40
Tangue Muweh <atmuweh@gmail.com>
2018-04-27 10:53:17 +0200

    data types and arithmetic operations

---
+++ datatypes.py
@@ -0,0 +1,36 @@
+
+#int datatype
+five = 5
+
+#float
+afloat = 5.5
+
+#string
+name = "tmuweh is my username"
+
+#some arithmetic operation
+
+#addition
+print(five + five)
+
+#multiplication
+print(five * five)
+
+#division
+print(five / five)
+
+#power
+print(five ** five)
+
+#remainder
+print(five % five)
+
+#troncate
+print(five // afloat)
+
+#concatenation of strings
+
+name = name + " use is to login"
+print(name)
+
+#objects to be convered in intermediate python cheers
