98c5baec7df181bcba3694fe5f7c1609f2d4c2d7
Tangue Muweh <35699075+tmuweh@users.noreply.github.com>
2018-06-01 00:32:48 +0200

    Add files via upload
    list datatype in python. mutable implimentation

---
+++ list.py
@@ -0,0 +1,15 @@
+
+sample = [1, "Python", 0, 5]
+print(sample)
+print(type(sample))
+
+print(sample[0])
+print(sample[-2])
+print(sample[-4])
+#modify
+sample[-4] = "changed"
+
+print(sample[-4])
+print(sample[0])
+#type of member
+print(type(sample[0]))
